2110a781616a796ab191477b44a0615bde4b6edc
mohanrajs7 <mohanrajs7@outlook.com>
2023-09-17 13:08:52 +0530

    Variable Declaration

---
+++ Java Projects/src/Basic_Programs/Variable_Declaration.java
@@ -0,0 +1,21 @@
+package Basic_Programs;
+
+public class Variable_Declaration {
+
+	public static void main(String[] args) {
+		//Variable_Declaration
+		int _a=6;
+		System.out.println(_a);
+		int $a=10;
+		System.out.println($a);
+		int a1=30;
+		System.out.println(a1);
+		int _6a=90;
+		System.out.println(_6a);
+		//1a,-,',a+b,My A---these are the errors in variable declaration
+		
+		
+
+	}
+
+}
