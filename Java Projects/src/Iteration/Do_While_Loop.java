228c9cd98e12597ec46c4aadcf3503f9c5a6f4e5
mohanrajs7 <mohanrajs7@outlook.com>
2023-09-21 22:24:51 +0530

    Loops

---
+++ Java Projects/src/Iteration/Do_While_Loop.java
@@ -0,0 +1,18 @@
+package Iteration;
+
+public class Do_While_Loop {
+
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			//On First Time It Does not checks condition
+			//After Execution one time then only it checks Condition
+			System.out.println("Java Program "+i+" Time");
+			i++;
+		} while(i<=5) ;
+			
+			
+
+	}
+
+}
---
+++ Java Projects/src/Iteration/While_Loop.java
@@ -0,0 +1,15 @@
+package Iteration;
+
+public class While_Loop {
+
+	public static void main(String[] args) {
+		int a=1;
+		while(a<7) {
+			System.out.println("Java Program Runs "+a+" Time");
+			a++;
+		}
+		
+
+	}
+
+}
