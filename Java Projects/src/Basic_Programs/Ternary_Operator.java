6b140cfb38a496a4d99dd4a9f332e15a7dd92c14
mohanrajs7 <mohanrajs7@outlook.com>
2023-09-21 23:03:22 +0530

    Ternary Operator

---
+++ Java Projects/src/Basic_Programs/Ternary_Operator.java
@@ -0,0 +1,12 @@
+package Basic_Programs;
+
+public class Ternary_Operator {
+
+	public static void main(String[] args) {
+		int a=20,b=200;
+		String c=a>b?"True":"False";
+		System.out.println(c);
+
+	}
+
+}
