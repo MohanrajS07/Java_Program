a3ad9315f33d1fa54d8495613f93e86a7e29e8c0
mohanrajs7 <mohanrajs7@outlook.com>
2023-10-09 18:45:40 +0530

    Constructor

---
+++ Java Projects/src/Constructor_In_Java/Default_Constructor.java
@@ -0,0 +1,19 @@
+package Constructor_In_Java;
+
+
+
+class Constructor{
+	Constructor(){
+		System.out.println("Default Constructor is called while creating Object.");
+	}
+}
+
+
+public class Default_Constructor {
+
+	public static void main(String[] args) {
+		Constructor c=new Constructor();
+
+	}
+
+}
